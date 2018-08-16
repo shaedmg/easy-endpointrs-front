c59b6b0eb106315e1c3ea043331cc8d6cd9bc76b
Isma <ismaelbg_99@hotmail.com>
2018-08-16 12:31:10 +0100

    add register

---
+++ front/src/components/register.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="register">
+    <form action="">
+      <label>Name</label>
+      <input type="text">
+      <label>Email</label>
+      <input type="text">
+      <label>Username</label>
+      <input type="text">
+      <label>Password</label>
+      <input type="password">
+      <label>Confirm Password</label>
+      <input type="password">
+    </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'register'
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+form {
+  display:flex;
+  flex-direction: column;
+}
+</style>
