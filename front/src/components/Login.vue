e3a29b1360459163d6bbf45cc13ac6169a045c6b
Juan Antonio <juanantonio19344@hotmail.com>
2018-08-16 12:36:20 +0100

    Add login

---
+++ front/src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="main-div">
+      <input type="email" placeholder="Email..." id="email_field" />
+      <input type="password" placeholder="Password..." id="password_field" />
+      <button onclick="login()">Login to Account</button>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
